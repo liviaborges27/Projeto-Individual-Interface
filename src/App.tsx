9856792cc4e0431115391cbb6eb359ff164408ff
Lívia Borges Matos <livia.matos2721@gmail.com>
2026-09-04 08:50:31 -0300

    feat: atualiza App.tsx com as rotas de Produtos, Categorias e Movimentações

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import PHome from "./pages/PHome/PHome";
+import PCategorias from "./pages/PCategorias/PCategorias";
+import PMovimentacoes from "./pages/PMovimentacao/PMovimentacao";
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <PHome />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Rota Principal: Gestão de Produtos */}
+        <Route path="/" element={<PHome />} />
+
+        {/* Gestão de Categorias */}
+        <Route path="/categorias" element={<PCategorias />} />
+
+        {/* Histórico e Registro de Movimentações */}
+        <Route path="/movimentacoes" element={<PMovimentacoes />} />
+
+        {/* Redirecionamento para rotas inexistentes */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
